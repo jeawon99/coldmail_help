725258216b52441057f7038d6bf7d7acbcd8a71e
altbit1999 <154398570+altbit1999@users.noreply.github.com>
2026-01-23 02:59:56 +0900

    Add SECURE_PROXY_SSL_HEADER for HTTPS detection behind Nginx

--- coldmail_project/settings/prod.py
+++ coldmail_project/settings/prod.py
@@ -27,6 +27,7 @@ DATABASES = {
 }
 
 # Security settings
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = os.getenv('SECURE_SSL_REDIRECT', 'False') == 'True'
 SESSION_COOKIE_SECURE = os.getenv('SESSION_COOKIE_SECURE', 'False') == 'True'
 CSRF_COOKIE_SECURE = os.getenv('CSRF_COOKIE_SECURE', 'False') == 'True'
